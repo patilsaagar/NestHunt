e1b078fa8142451e50b4972ba8d6a8409368fe79
sagar.patil <sagar.patil@thoughtworks.com>
2025-02-06 09:37:42 +0530

    Added login screen

--- app/index.tsx
+++ app/index.tsx
@@ -1,17 +1,44 @@
-import { Text, View } from "react-native";
+import {
+  Image,
+  SafeAreaView,
+  ScrollView,
+  Text,
+  TouchableOpacity,
+  View,
+} from "react-native";
+import images from "@/constants/images";
+import icons from "@/constants/icons";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text className="font-bold font-lg">
-        Edit app/index.tsx to edit this screen.
-      </Text>
-    </View>
+    <SafeAreaView className="bg-white h-full">
+      <ScrollView contentContainerClassName="h-full">
+        <Image
+          source={images.onboarding}
+          resizeMode="contain"
+          className="w-full h-4/6"
+        />
+        <View>
+          <Text className="uppercase text-base text-center text-black-400">
+            Welcome to your Nesthunt
+          </Text>
+          <Text className="text-black-400 text-center text-3xl font-rubik-bold">
+            Let's get start
+          </Text>
+          <TouchableOpacity className="bg-white shadow-md rounded-full p-6 m-6">
+            <View className="flex flex-row items-center justify-center">
+              <Image
+                source={icons.google}
+                className="w-5 h-5"
+                resizeMethod="contain"
+              ></Image>
+              <Text className="ml-2 text-lg text-black-300 font-rubik-medium">
+                Continue with Google
+              </Text>
+            </View>
+          </TouchableOpacity>
+        </View>
+      </ScrollView>
+    </SafeAreaView>
   );
 }
